27fdf82636d056d5330ec916a04e89d738e320e1
Aaron Pelto <ajpelto@gmail.com>
2023-10-11 22:47:51 -0400

    Update Main.java

--- Program 4/src/Main.java
+++ Program 4/src/Main.java
@@ -1,3 +1,15 @@
+// https://elearning.delta.edu/d2l/le/dropbox/2970298/261322/DownloadAttachment?fid=9504649
+// http://weather.uwyo.edu/upperair/sounding.html
+/*
+Write a Java program that will determine the risk of severe weather at a given weather station by utilizing measurements
+taken from a weather balloon. Weather balloons are used to observe upper air measurements.
+There are several severe weather indexes used by meteorologists. Two are included in this assignment. Each include a
+simple arithmetic formula and are defined to calculate values based on patterns and conditions likely to produce severe
+weather. They offer a forecaster a quick number that can be referenced to assist in judging weather risks on a given day.
+These measurements taken via weather balloons are not taken at standard heights, but instead at standard pressure
+levels (in the unit of millibars). The severe weather indexes your program will calculate require the following values (℃):
+
+ */
 
 public class Main {
     public static void main(String[] args) {

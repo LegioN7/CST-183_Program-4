2197512c9d679f72d86dff976f83b7d607bd1c4f
Aaron Pelto <ajpelto@gmail.com>
2023-10-13 00:52:46 -0400

    Updates for Methods
    Missing Methods added

--- Program 4/src/severeWeatherRisk.java
+++ Program 4/src/severeWeatherRisk.java
@@ -19,23 +19,79 @@ import java.util.Scanner;
 //T850 = 20 ℃, Td850 = 15 ℃, T700 = 3 ℃, Td700 = 1 ℃, and T500 = –9 ℃
 public class severeWeatherRisk {
 
+    // static values for the temperatures
+    public static final double temperatureEightHundredFiftyMin = -40;
+    public static final double temperatureEightHundredFiftyMax = 40;
+    public static final double temperatureDewPointEightHundredFiftyMin = -40;
+    public static final double temperatureSevenHundredMin = -60;
+    public static final double temperatureSevenHundredMax = 10;
+    public static final double temperatureDewPointSevenHundredMin = -60;
+    public static final double temperatureFiveHundredMin = -50;
+    public static final double temperatureFiveHundredMax = 0;
 
+    // Calculates the Total Total Index from the uadata.txt file
     public static double totalTotalIndexCalc(double temperatureEightHundredFifty, double dewPointEightHundredFifty, double temperatureFiveHundred) {
+        //I could inline this code and remove the redundancy
         double totalTotalIndex = temperatureEightHundredFifty + dewPointEightHundredFifty - 2 * (temperatureFiveHundred);
 
         return totalTotalIndex;
     }
 
+    // Calculates the K Index from the uadata.txt file
     public static double kIndexCalc(double temperatureEightHundredFifty, double dewPointEightHundredFifty, double temperatureSevenHundred, double dewPointSevenHundred, double temperatureFiveHundred) {
+        //I could inline this code and remove the redundancy
         double kIndex = temperatureEightHundredFifty + dewPointEightHundredFifty - temperatureFiveHundred - (temperatureSevenHundred - dewPointSevenHundred);
         return kIndex;
     }
 
+    public static String totalTotalWeatherRiskCalc(double totalTotalIndex) {
+
+        String totalTotalForecast = null;
+
+        if (totalTotalIndex < 44) {
+            totalTotalForecast = "Thunderstorms Unlikely";
+        } else if (totalTotalIndex >= 44 && totalTotalIndex <= 45) {
+            totalTotalForecast = "Isolated Moderate Thunderstorms";
+        } else if (totalTotalIndex >= 46 && totalTotalIndex <= 47) {
+            totalTotalForecast = "Scattered Moderate, Few Heavy Thunderstorms";
+        } else if (totalTotalIndex >= 48 && totalTotalIndex <= 49) {
+            totalTotalForecast = "Scattered Moderate, Few Heavy, Isolated Severe Thunderstorms";
+        } else if (totalTotalIndex >= 50 && totalTotalIndex <= 51) {
+            totalTotalForecast = "Scattered Heavy, Few Severe Thunderstorms, Isolated Tornadoes";
+        } else if (totalTotalIndex >= 52 && totalTotalIndex <= 55) {
+            totalTotalForecast = "Scattered to Numerous Heavy, Few to Scattered Severe Thunderstorms, Isolated Tornadoes";
+        } else if (totalTotalIndex > 55) {
+            totalTotalForecast = "Numerous Heavy, Scattered Severe Thunderstorms, Few to Scattered Tornadoes";
+        }
+
+        return totalTotalForecast;
+    }
+
+    public static String kIndexWeatherRiskCalc(double kIndexPlace) {
+
+        String kIndexForecast = null;
+
+        if (kIndexPlace < 20) {
+            kIndexForecast = "Thunderstorms Unlikely";
+        } else if (kIndexPlace >= 20 && kIndexPlace <= 25) {
+            kIndexForecast = "Isolated Thunderstorms";
+        } else if (kIndexPlace >= 26 && kIndexPlace <= 30) {
+            kIndexForecast = "40% - 60% chance of thunderstorms";
+        } else if (kIndexPlace >= 31 && kIndexPlace <= 35) {
+            kIndexForecast = "60% - 80% chance of thunderstorms, some severe";
+        } else if (kIndexPlace >= 36 && kIndexPlace <= 40) {
+            kIndexForecast = "80% - 90% chance of heavy thunderstorms, some severe";
+        } else if (kIndexPlace > 40) {
+            kIndexForecast = "Almost 100% chance for thunderstorms, some severe";
+        }
+
+        return kIndexForecast;
+    }
 
     // This validates the input of all the temperature
-    public static double validateTemperature (double temperature, double min,double max){
-        if (temperature >= min && temperature <= max)
-        {
+    // Dewpoint can not be greater than the temperature in its value (i.e. 850 dewpoint can not be greater than 850 temperature)
+    public static double validateTemperature(double temperature, double min, double max) {
+        if (temperature >= min && temperature <= max) {
             return temperature;
         } else {
             throw new IllegalArgumentException("Invalid temperature. Temperature must be between " + min + " and " + max + " Celsius.");
@@ -45,24 +101,36 @@ public class severeWeatherRisk {
     public static void main(String[] args) {
 
         // All Temperature Values are in Celsius
+        // Set the variables for the data that comes from the uadata.txt file
         double temperatureEightHundredFifty = 0; //T850
         double dewPointEightHundredFifty = 0; //Td850
         double temperatureSevenHundred = 0; //T700
         double dewPointSevenHundred = 0; //Td700
         double temperatureFiveHundred = 0; //T500
 
-        String totalTotalForecast = "";
-        String kIndexForecast = "";
+        // Placeholder string for the Weather Risks Texts
+
+
+        // Read the uadata.txt file
+        // Data comes in individual lines
+
+        // Example Start
+        // value
+        // value
+        // value
+        // value
+        // value
+        // Example End
 
         try {
             File myObj = new File("uadata.txt");
             Scanner dataReader = new Scanner(myObj);
 
-            temperatureEightHundredFifty = validateTemperature(dataReader.nextDouble(), -40, 40);
-            dewPointEightHundredFifty = validateTemperature(dataReader.nextDouble(), -40, temperatureEightHundredFifty);
-            temperatureSevenHundred = validateTemperature(dataReader.nextDouble(), -60, 10);
-            dewPointSevenHundred = validateTemperature(dataReader.nextDouble(), -60, temperatureSevenHundred);
-            temperatureFiveHundred = validateTemperature(dataReader.nextDouble(), -50, 0);
+            temperatureEightHundredFifty = validateTemperature(dataReader.nextDouble(), temperatureEightHundredFiftyMin, temperatureEightHundredFiftyMax);
+            dewPointEightHundredFifty = validateTemperature(dataReader.nextDouble(), temperatureDewPointEightHundredFiftyMin, temperatureEightHundredFifty);
+            temperatureSevenHundred = validateTemperature(dataReader.nextDouble(), temperatureSevenHundredMin, temperatureSevenHundredMax);
+            dewPointSevenHundred = validateTemperature(dataReader.nextDouble(), temperatureDewPointSevenHundredMin, temperatureSevenHundred);
+            temperatureFiveHundred = validateTemperature(dataReader.nextDouble(), temperatureFiveHundredMin, temperatureFiveHundredMax);
 
 
             // Calculation Holders for Total Total Index and K Index
@@ -70,38 +138,14 @@ public class severeWeatherRisk {
             double kIndexPlace = kIndexCalc(temperatureEightHundredFifty, dewPointEightHundredFifty, temperatureSevenHundred, dewPointSevenHundred, temperatureFiveHundred);
 
 
-            if (totalTotalIndex < 44) {
-                totalTotalForecast = "Thunderstorms Unlikely";
-            } else if (totalTotalIndex >= 44 && totalTotalIndex <= 45) {
-                totalTotalForecast = "Isolated Moderate Thunderstorms";
-            } else if (totalTotalIndex >= 46 && totalTotalIndex <= 47) {
-                totalTotalForecast = "Scattered Moderate, Few Heavy Thunderstorms";
-            } else if (totalTotalIndex >= 48 && totalTotalIndex <= 49) {
-                totalTotalForecast = "Scattered Moderate, Few Heavy, Isolated Severe Thunderstorms";
-            } else if (totalTotalIndex >= 50 && totalTotalIndex <= 51) {
-                totalTotalForecast = "Scattered Heavy, Few Severe Thunderstorms, Isolated Tornadoes";
-            } else if (totalTotalIndex >= 52 && totalTotalIndex <= 55) {
-                totalTotalForecast = "Scattered to Numerous Heavy, Few to Scattered Severe Thunderstorms, Isolated Tornadoes";
-            } else if (totalTotalIndex > 55) {
-                totalTotalForecast = "Numerous Heavy, Scattered Severe Thunderstorms, Few to Scattered Tornadoes";
-            }
+            // Severe Weather Texts for Total Total Index
+            String totalTotalForecast = totalTotalWeatherRiskCalc(totalTotalIndex);
+            String kIndexForecast = kIndexWeatherRiskCalc(kIndexPlace);
 
-            // K Index Forecast
-            if (kIndexPlace < 20) {
-                kIndexForecast = "Thunderstorms Unlikely";
-            } else if (kIndexPlace >= 20 && kIndexPlace <= 25) {
-                kIndexForecast = "Isolated Thunderstorms";
-            } else if (kIndexPlace >= 26 && kIndexPlace <= 30) {
-                kIndexForecast = "40% - 60% chance of thunderstorms";
-            } else if (kIndexPlace >= 31 && kIndexPlace <= 35) {
-                kIndexForecast = "60% - 80% chance of thunderstorms, some severe";
-            } else if (kIndexPlace >= 36 && kIndexPlace <= 40) {
-                kIndexForecast = "80% - 90% chance of heavy thunderstorms, some severe";
-            } else if (kIndexPlace > 40) {
-                kIndexForecast = "Almost 100% chance for thunderstorms, some severe";
-            }
+            // Severe Weather Texts for K Index
 
 
+            // Dialog Panels for the output
             JOptionPane.showMessageDialog(null, "Total Total Placeholder: " + totalTotalIndex + "\n" + "Total Total Index Forecast: " + totalTotalForecast);
             JOptionPane.showMessageDialog(null, "K Index Placeholder: " + kIndexPlace + "\n" + "K Index Forecast: " + kIndexForecast);
 
